1ba57a560f06c63afad7a1c2d55bf4f3b78470d7
Mario Gamez <mariogamez1999@gmail.com>
2022-03-03 17:34:51 -0700

    Fin seccion 7

---
+++ src/app/services/productos.service.ts
@@ -0,0 +1,79 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Producto } from '../interfaces/producto.interface';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductosService {
+
+  cargando = true;
+  productos: Producto[] = [];
+  productosFiltrado: Producto[] = [];
+
+
+  constructor( private http: HttpClient ) {
+
+    this.cargarProductos();
+
+  }
+
+
+  private cargarProductos() {
+
+    return new Promise( ( resolve, reject ) => {
+
+      this.http.get('https://carpinteriaelmony-default-rtdb.firebaseio.com/productos_idx.json')
+      .subscribe( (resp: Producto[]) => {
+
+        this.productos = resp;
+        this.cargando = false;
+        resolve();
+      });
+    })
+  }
+
+  getProducto( id: string ){
+    return this.http.get(`https://carpinteriaelmony-default-rtdb.firebaseio.com/productos/${ id }.json`)
+  }
+
+  buscarProducto( termino: string){
+
+    if ( this.productos.length === 0) {
+      //cargar productos
+      this.cargarProductos().then ( () => {
+        //ejecutar despues de tener los productos
+        // aplicar filtro
+        this.filtrarProductos( termino );
+      });
+
+    } else {
+      //aplicar el filtro
+      this.filtrarProductos( termino );
+    }
+
+    
+
+  }
+
+  private filtrarProductos(termino: string){
+    this.productosFiltrado = [];
+
+    termino = termino.toLocaleLowerCase();
+
+    this.productos.forEach( prod => {
+
+      const tituloLower = prod.titulo.toLocaleLowerCase();
+      const categoriaLower = prod.categoria.toLocaleLowerCase();
+      
+
+      if ( categoriaLower.indexOf( termino ) >= 0 || tituloLower.indexOf( termino ) >= 0){
+        this.productosFiltrado.push( prod );
+      }
+    })
+
+
+  }
+
+}
